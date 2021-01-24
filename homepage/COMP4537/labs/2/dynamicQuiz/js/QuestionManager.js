1e54ad36a80bf6a5293014fa28155af107070dbc
Harman Sandhu <hrrsandhu6@gmail.com>
2021-01-23 16:28:49 -0800

    refactored js

---
+++ homepage/COMP4537/labs/2/dynamicQuiz/js/QuestionManager.js
@@ -0,0 +1,24 @@
+ class QuestionManager{
+        constructor(){
+            let questions = localStorage.getItem("qlist")
+            if(questions == null){
+                localStorage.setItem("qlist", JSON.stringify({}))
+            }
+            this.qlist = JSON.parse(localStorage.getItem("qlist"));
+        }
+
+        render(){
+            for(let id in this.qlist){
+                Question.render(id, true)
+            }
+        }
+
+        count(){
+            let count = 0;
+            for(let prop in this.qlist){
+                if(this.qlist.hasOwnProperty(prop))
+                    count++
+            }
+            return count;
+        }
+    }
