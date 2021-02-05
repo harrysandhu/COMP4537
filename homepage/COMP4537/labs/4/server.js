b6effff902e56840723fded70369f32c46beab1b
sarahesla <sarah.eslamdoust@gmail.com>
2021-02-04 22:15:48 -0800

    Update server.js

--- homepage/COMP4537/labs/4/server.js
+++ homepage/COMP4537/labs/4/server.js
@@ -1,8 +1,14 @@
-let http = require('http');
-http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-type':'text/plain'});
-    response.write("Response's coming from server");
-    response.end();
-}
-).listen(8080);
-console.log("listening");
+const http = require('http');
+const utils = require('./modules/utils');
+const url = require('url');
+
+http.createServer(function(req, res) {
+    let adr = url.parse(req.url, true);
+    let name = adr.query.name;
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("Hello " + name + ", here is the server's current date and time: " + utils.getDate())
+    res.end();
+}).listen(8080);
+
+console.log("LISTENING ON 8080");
+// server.listen();
