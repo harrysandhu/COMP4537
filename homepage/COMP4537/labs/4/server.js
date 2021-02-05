2268a7a141abbb80b0cc97d38f33b137232b4c71
sarahesla <sarah.eslamdoust@gmail.com>
2021-02-04 22:46:57 -0800

    Update server.js

--- homepage/COMP4537/labs/4/server.js
+++ homepage/COMP4537/labs/4/server.js
@@ -1,13 +1,39 @@
 const http = require('http');
-const utils = require('./modules/utils');
 const url = require('url');
+const fs = require('fs');
+const utils = require('./modules/utils');
+
 
 http.createServer(function(req, res) {
     let adr = url.parse(req.url, true);
-    let name = adr.query.name;
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("Hello " + name + ", here is the server's current date and time: " + utils.getDate())
-    res.end();
+    
+    if (adr.pathname == "/getDate") {
+        let name = adr.query.name;
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write("Hello " + name + ", here is the server's current date and time: " + utils.getDate())
+        res.end();
+    }
+
+    else if (adr.pathname == "/writeFile") {
+        let content = adr.query.text;
+        fs.appendFile('file.txt', content, function (err) {
+            if (err) throw err;
+            console.log("Saved!");
+        });
+    }
+
+    else if (adr.pathname == "/readFile/file.txt") {
+        fs.readFile("file.txt", function (err, data) {
+            if (err) {
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                return res.end(adr.pathname + "404 Not Found!");
+            }
+
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(date);
+            return res.end();
+        });
+    }
 }).listen(8080);
 
 console.log("LISTENING ON 8080");
