ee15cb0589ce6ef357d4ade8603ae7b75e3c6226
Harman Sandhu <hrrsandhu6@gmail.com>
2021-02-05 09:27:45 -0800

    filetypes

---
+++ homepage/filetypes.js
@@ -0,0 +1,13 @@
+const filetypes = {
+    'html' : "text/html",
+    'jpg': "image/jpg",
+    'jpeg':"image/jpeg",
+    'png':"image/png",
+    'svg': "image/svg",
+    'js': "application/javascript",
+    'css': "text/css",
+    "json": "application/json"
+
+}
+
+module.exports = filetypes
---
+++ homepage/x.js
@@ -0,0 +1,4 @@
+m = [1, 2, 5]
+for(let i in m){
+    console.log(i)
+}
