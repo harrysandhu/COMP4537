9fe8437b74d045a4020210df0c37d82f41655857
Harman Sandhu <hrrsandhu6@gmail.com>
2021-02-05 17:57:00 -0800

    file path fix

--- homepage/app.js
+++ homepage/app.js
@@ -30,7 +30,7 @@ stonk.get(path.join(L4 + "/getDate"), (req, res) => {
 stonk.get(path.join(L4 + "/writeFile"), (req, res) => {
     try{
         let content = stonk.query.text;
-        fs.appendFile('file.txt', content, function (err) {
+        fs.appendFile('./COMP4537/labs/4/readFile/file.txt', content, function (err) {
             if (err) throw err;
             stonk.html("<h1>SAVED!</h1>", 200)
             console.log("Saved!");
