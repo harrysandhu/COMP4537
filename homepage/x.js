9d5822846e0da51e11b35c2a7172f718ef4429c5
Harman Sandhu <hrrsandhu6@gmail.com>
2021-02-12 17:52:51 -0800

    x

--- homepage/x.js
+++ homepage/x.js
@@ -1,4 +1,15 @@
-m = [1, 2, 5]
-for(let i in m){
-    console.log(i)
-}
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  user: "doadmin",
+  host: "db-mysql-nyc1-53815-do-user-1754324-0.b.db.ondigitalocean.com",
+  password: "x4eggesk315ahf8c",
+  port:25060,
+  insecureAuth : true,
+  database: "defaultdb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
