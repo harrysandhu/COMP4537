f5de64bb9d96d95b0bc1f8649018d007674d18f5
Harman Sandhu <hrrsandhu6@gmail.com>
2021-03-06 20:34:24 -0800

    POST for stonk

--- homepage/stonk.js
+++ homepage/stonk.js
@@ -109,12 +109,15 @@ class Stonk{
                     console.log("yo", pathname)
                     this.post_routes[pathname]['f'](req, res)
                     let body = ''
+                    let that = this
                     req.on('data', (dat) =>{
+                        console.log(dat)
                         body += dat
                         if (body.length > 1e6) req.connection.destroy()
                     })
                     req.on("end", () => {
-                        this.data = qs.parse(body)
+                        console.log(body)
+                        that.data = JSON.parse(body)
                     })
                 }
                 
