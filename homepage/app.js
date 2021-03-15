a42e82dde55ee88cfe9e7ab6d0054e048ffdcfe8
Harman Sandhu <hrrsandhu6@gmail.com>
2021-03-14 21:54:39 -0700

    questions

--- homepage/app.js
+++ homepage/app.js
@@ -1,7 +1,7 @@
 let http = require("http")
 let fs = require("fs")
 let path = require("path")
-let appDir = path.dirname(require.main.filename)+ "/"
+let appDir = path.join(path.dirname(require.main.filename), "/")
 let Stonk = require("./stonk.js")
 const L4 = "/COMP4537/labs/4";
 const utils = require('./' + L4 + '/modules/utils');
@@ -14,7 +14,7 @@ const con = mysql.createConnection({
   password: "iz1udqdoydn51yas",
   port:25060,
   insecureAuth : true,
-  database: "defaultdb"
+  database: "quiz"
 });
 
 
@@ -89,6 +89,65 @@ stonk.get(path.join(L(5), "writeDB"), (req, res) => {
     }
 })
 
+
+stonk.post(path.join(L(6), "question"), (req, res) => {
+    try{
+        let {question} = stonk.data
+        console.log(question)
+        let sql = "SELECT * FROM questions WHERE id = ? "
+        con.query(sql,[question.id], function(err,  result){
+            if (err) throw 'error2';
+            console.log("RESULT QUES", result)
+       
+            if(result.length == 0){
+                let sql = "INSERT INTO questions (id, question) VALUES ('"+question.id+"','"+question['question']+"')"
+      
+                con.query(sql, function(err,  result){
+                    if (err) throw 'error1';
+                    console.log("record"+ JSON.stringify(result))
+                })
+              
+                console.log("THIS IS COOL")
+                sql = "SELECT * FROM options WHERE questionId = ? "
+                con.query(sql,[question.id], function(err,  result){
+                    if (err) throw 'error2';
+                    console.log("RESULT QUES", result)
+                        if(result.length == 0){
+                            let sql = "INSERT INTO options (questionID, answer, isCorrect) VALUES ?";
+                            let values = []
+                            let {id, options, answer} = question
+                            let isCorrect = 0
+                            for(let i = 0; i < 4; i++){
+                                let f = []
+                                isCorrect = 0
+                                if(answer == options[i]){
+                                    isCorrect = 1
+                                }
+                                f.push(id)
+                                f.push(options[i])
+                                f.push(isCorrect)
+                                values.push(f)
+                            }
+                            console.log(values)
+                            con.query(sql, [values], function(err,  result){
+                                if (err) throw 'err2';
+                                console.log("record"+ JSON.stringify(result))
+                            })
+                            stonk.json(JSON.stringify({"message": "it worked!!"}), 200)
+                        }else{
+                            throw 'error: duplicate question 1'
+                        }
+            })  
+            }else{
+                throw 'error: duplicate question 2'
+            }
+            
+        })
+        
+    }catch(e){
+        stonk.json(JSON.stringify({"error": e}), 400)
+    }
+})
 
 // let it rip
 stonk.rip(4040, () => {
