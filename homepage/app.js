aacd63361d8074973081e08ade80df805f927e28
Harmandeep <hrrsandhu6@gmail.com>
2021-03-20 23:01:05 -0600

    Sarah

--- homepage/app.js
+++ homepage/app.js
@@ -17,6 +17,13 @@ const con = mysql.createConnection({
   database: "quiz"
 });
 
+const dbConnection = mysql.createConnection({
+  host: "s11.fcomet.com",
+  user: "sarahesl_admin",
+  password: "thisisapassword",
+  database: "sarahesl_isa-quiz"
+});
+
 
 
 let stonk = new Stonk()
@@ -176,6 +183,58 @@ stonk.get(path.join(L(6), "questions"), (req, res) => {
         stonk.json(JSON.stringify({"result": data}), 200)
     })
 })
+
+
+
+stonk.get("/s/questions", (req, res) => {
+  try {
+    let query = "SELECT * FROM questions JOIN options ON questions.id = options.questionID";
+    dbConnection.query(query, function(err, result, fields) {
+      console.log(result);
+      stonk.json(JSON.stringify(result), 200);
+    });
+  }
+
+  catch(e) {
+    stonk.html("400", 400);
+  }
+});
+
+stonk.post("/s/addQuestion", (req, res) => {
+  try {
+    let data = stonk.data;
+    console.log(data);
+    let addQuestion = "INSERT INTO questions (id, question) VALUES ('"+data.id+"', '"+data.question+"')";
+    dbConnection.query(addQuestion, function(err, result, fields) {
+      let addOptions = "INSERT INTO options (questionID, answer, isCorrect) VALUES ?";
+      let values = []
+      let {id, options, isCorrect} = data
+      for(let i = 0; i < 2; i++){
+          let f = []
+          f.push(id)
+          f.push(options[i])
+          f.push(isCorrect[i])
+          values.push(f)
+      }
+      console.log(values)
+      dbConnection.query(addOptions, [values], function(err,  result){
+          if (err) throw 'err2';
+          console.log("record"+ JSON.stringify(result))
+      })
+      stonk.json(JSON.stringify(result), 200);
+    });
+  }
+
+  catch(e) {
+    stonk.html("400", 400);
+  }
+})
+
+dbConnection.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
 
 // let it rip
 stonk.rip(4040, () => {
