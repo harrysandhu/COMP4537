9dbcb3eed4a2c5e091f66e80d2a2c4479a430ff7
Harman Sandhu <hrrsandhu6@gmail.com>
2021-04-03 12:12:38 -0700

    remove result wrapper

--- tproj/server/src/DBManager.js
+++ tproj/server/src/DBManager.js
@@ -1,5 +1,5 @@
 import Result from "./Result";
-
+let mysql = require("mysql")
 /**
  * DBManager provides an interface for all use cases.
  * 
@@ -14,16 +14,16 @@ export default class DBManager{
             try{
                 this.con.connect()
                 let sql = "INSERT INTO ?? (??) VALUES (?)"
-                sql = mysql.format(sql, table, columns, values)
+                sql = mysql.format(sql, [table, columns, values])
                 this.con.query(sql, (error, results, fields) =>{
-                    if (error) reject(Result.Error(error))
+                    if (error) reject(error)
                     else{
-                        resolve(Result.Success(results))
+                        resolve(results)
                     }
                 })
 
             }catch(e){
-                reject(Result.Error("Database error: cannot register admin"))
+                reject("Database error: cannot register admin")
             }finally{
                 this.con.end()
             }  
