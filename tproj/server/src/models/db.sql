d04f935ad152392ef849886c9490ccfe40627229
Harman Sandhu <hrrsandhu6@gmail.com>
2021-04-03 22:24:35 -0700

    api_request model

--- tproj/server/src/models/db.sql
+++ tproj/server/src/models/db.sql
@@ -1,8 +1,19 @@
 CREATE TABLE IF NOT EXISTS admin(
     admin_id VARCHAR (100) NOT NULL UNIQUE,
-    email VARCHAR(255) NOT NULL UNIQUE,
+    username VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(511) NOT NULL,
     salt VARCHAR(40) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(admin_id)
+);
+
+
+
+CREATE TABLE IF NOT EXISTS api_request(
+    request_id INT NOT NULL AUTO_INCREMENT,
+    admin_id VARCHAR (100) NOT NULL,
+    endpoint VARCHAR (100) NOT NULL,
+    request_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(request_id),
+    FOREIGN KEY (admin_id) REFERENCES admin(admin_id)
 );
