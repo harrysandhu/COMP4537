64875faafa4ee0d1b68f0d5e75ea3c9de7fa49c9
Harman Sandhu <hrrsandhu6@gmail.com>
2021-04-04 20:54:59 -0700

    proxy middleware

--- tproj/server/x.js
+++ tproj/server/x.js
@@ -1,5 +1,8 @@
+let x = {
+    s : "s"
+}
 
-let mysql = require("mysql")
-let sql = "INSERT INTO ?? (??) VALUES (?)"
-sql = mysql.format(sql, ["admin", ["email", "password_hash"], ["dddd", "fdfd"]])
-console.log(sql)
+
+if("s" in x){
+    console.log("Ss")
+}
