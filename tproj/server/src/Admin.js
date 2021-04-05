f2dca90ae910a8c9239b4be0488242a1bc2618fb
Harman Sandhu <hrrsandhu6@gmail.com>
2021-04-04 20:21:27 -0700

    fetch endpoint data

--- tproj/server/src/Admin.js
+++ tproj/server/src/Admin.js
@@ -78,5 +78,23 @@ export default class Admin{
         }
     }
     
+    static async fetch_endpoint_data(admin_id, db){
+        try{
+            let api_data = await db.select_grouped("api_request", 
+            ["endpoint", "method"], 
+            {"admin_id": admin_id},
+            ["endpoint", "method"]
+            )
+            return Result.Success(api_data)
+        }catch(e){
+            console.log("WE HERE")
+            console.log(e)
+            if("details" in e){
+                e = e.details
+                e.code = "SQL"
+            }    
+            throw Result.Error(e)
+        }
+    }
     
 }
