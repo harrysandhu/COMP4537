8f85e8da6b4bd6d35ec5dbdffe29605dc104a31f
Harman Sandhu <hrrsandhu6@gmail.com>
2021-04-10 12:09:03 -0700

    end db thread

--- tproj/server/api/v1/api.js
+++ tproj/server/api/v1/api.js
@@ -7,6 +7,7 @@ import DBManager from '../../src/DBManager'
 import { verify } from 'crypto'
 import Helper from '../../src/Helper'
 import User from '../../src/User'
+import Ledger from '../../src/Ledger'
 let path = require("path")
 let api = express.Router()
 let privateKey = fs.readFileSync("./src/security/private.key", "utf8");
@@ -16,212 +17,234 @@ let CONFIG = {
     user: "harryx",
     host: "db-mysql-nyc1-53815-do-user-1754324-0.b.db.ondigitalocean.com",
     password: "iz1udqdoydn51yas",
-    port:25060,
-    insecureAuth : true,
-    database: "tproj"
+    port: 25060,
+    insecureAuth: true,
+    database: "tproj",
+    max_connections: 40000
 }
 
 api.use(express.static(__dirname))
 
 /**Admin Resource
-* Endpoints:
-* 1. POST /admin/register
-* 2. POST /admin/login
-* 3. GET /admin
-*/
+ * Endpoints:
+ * 1. POST /admin/register
+ * 2. POST /admin/login
+ * 3. GET /admin
+ */
 api.post("/admin/register", async(req, res) => {
-    try{
+    try {
         // validate the request
-        let {username, password, repeat_password} = req.body
-        
+        let { username, password, repeat_password } = req.body
+
         //create a DBManager instance (dependecy injection)
         let db = new DBManager(CONFIG)
-        
+
         //validate, create, and register admin using db
-        let result = await Admin.create({username, password, repeat_password}, db)
-        if(result.get().isError){throw result}
+        let result = await Admin.create({ username, password, repeat_password }, db)
+        if (result.get().isError) { throw result }
         console.log(result)
-        //if error result will be thrown as an error
-        // by default, its a success.
+            //if error result will be thrown as an error
+            // by default, its a success.
         return res.status(200).json(result)
-        
-    }catch(e){
-        if("get" in e){
-            return res.status(400).json({error: e.get()})    
+
+    } catch (e) {
+        if ("get" in e) {
+            return res.status(400).json({ error: e.get() })
         }
         console.log(e)
-        return res.status(400).json({error: "Invalid Request."})
+        return res.status(400).json({ error: "Invalid Request." })
     }
 })
 
 
 
 api.post("/user/register", async(req, res) => {
-    try{
+
+    //create a DBManager instance (dependecy injection)
+    let db = new DBManager(CONFIG)
+    try {
         // validate the request
-        let {username, password, repeat_password} = req.body
-        
-        //create a DBManager instance (dependecy injection)
-        let db = new DBManager(CONFIG)
-        
+        let { username, password, repeat_password } = req.body
+
         //validate, create, and register admin using db
-        let result = await User.create({username, password, repeat_password}, db)
-        if(result.get().isError){throw result}
+        let result = await User.create({ username, password, repeat_password }, db)
+        if (result.get().isError) { throw result }
         console.log(result)
-        //if error result will be thrown as an error
-        // by default, its a success.
+            //if error result will be thrown as an error
+            // by default, its a success.
         return res.status(200).json(result)
-        
-    }catch(e){
-        if("get" in e){
-            return res.status(400).json({error: e.get()})    
+
+    } catch (e) {
+        if ("get" in e) {
+            return res.status(400).json({ error: e.get() })
         }
         console.log(e)
-        return res.status(400).json({error: "Invalid Request."})
+        return res.status(400).json({ error: "Invalid Request." })
+    } finally {
+        db.con.end()
     }
 })
 
-api.post("/admin/login", async(req, res) =>{
-    try{
-        let {username, password} = req.body
-        let db = new DBManager(CONFIG)
-        let result = await Admin.login({username, password}, db)
+api.post("/admin/login", async(req, res) => {
+    let db = new DBManager(CONFIG)
+    try {
+        let { username, password } = req.body
+        let result = await Admin.login({ username, password }, db)
+        db.con.end()
         return res.status(200).json(result)
-    }catch(e){
-        if("get" in e){
-            return res.status(400).json({error: e.get()})    
+    } catch (e) {
+        if ("get" in e) {
+            return res.status(400).json({ error: e.get() })
         }
         console.log(e)
-        return res.status(400).json({error: "Invalid Request."})
+        return res.status(400).json({ error: "Invalid Request." })
+    } finally {
+        db.con.end()
     }
 })
 
-api.post("/user/login", async(req, res) =>{
-    try{
-        let {username, password} = req.body
-        let db = new DBManager(CONFIG)
-        let result = await User.login({username, password}, db)
+api.post("/user/login", async(req, res) => {
+    let db = new DBManager(CONFIG)
+    try {
+        let { username, password } = req.body
+        let result = await User.login({ username, password }, db)
         return res.status(200).json(result)
-    }catch(e){
-        if("get" in e){
-            return res.status(400).json({error: e.get()})    
+    } catch (e) {
+        if ("get" in e) {
+            return res.status(400).json({ error: e.get() })
         }
         console.log(e)
-        return res.status(400).json({error: "Invalid Request."})
+        return res.status(400).json({ error: "Invalid Request." })
+    } finally {
+        db.con.end()
     }
 })
 
-api.get("/admin", Helper.verifyAuthToken, async(req, res)=>{
-    try{
+api.get("/admin", Helper.verifyAuthToken, async(req, res) => {
+    let db = new DBManager(CONFIG)
+    try {
         let currentUser = await Helper.jwtVerifyUser(req.token, publicKey)
-        // get user data and merge it into user object
-        let db = new DBManager(CONFIG)
+            // get user data and merge it into user object
         let api_data = await Admin.fetch_endpoint_data(currentUser.admin_id, db)
         console.log("api_data", api_data.get())
-        return res.json({...currentUser, "api_data":api_data.get()})
-    }catch(e){
-        if("get" in e){
-            return res.status(400).json({error: e.get()})    
+        return res.json({...currentUser, "api_data": api_data.get() })
+    } catch (e) {
+        if ("get" in e) {
+            return res.status(400).json({ error: e.get() })
         }
         console.log(e)
-        return res.status(400).json({error: "Invalid Request."})
+        return res.status(400).json({ error: "Invalid Request." })
+    } finally {
+        db.con.end()
     }
 })
 
-api.get("/user", 
-async (req, res, next) =>{
-    await Helper.verifyRequest(req, res, next, new DBManager(CONFIG))
-},
-Helper.verifyAuthToken,
-async (req, res) =>{
-    try{
-        let currentUser = await Helper.jwtVerifyUser(req.token, publicKey)
-        // get user data -> ledgers and shit and merge it into user object
-        return res.json({...currentUser})
-    }catch(e){
-        if("get" in e){
-            return res.status(400).json({error: e.get()})    
+api.get("/user",
+    async(req, res, next) => {
+        await Helper.verifyRequest(req, res, next, new DBManager(CONFIG))
+    },
+    Helper.verifyAuthToken,
+    async(req, res) => {
+        try {
+            let currentUser = await Helper.jwtVerifyUser(req.token, publicKey)
+                // get user data -> ledgers and shit and merge it into user object
+            return res.json({...currentUser })
+        } catch (e) {
+            if ("get" in e) {
+                return res.status(400).json({ error: e.get() })
+            }
+            console.log(e)
+            return res.status(400).json({ error: "Invalid Request." })
         }
-        console.log(e)
-        return res.status(400).json({error: "Invalid Request."})
     }
-}
 )
 
 
-api.get("/ledgers", 
-async (req, res, next) =>{
-    await Helper.verifyRequest(req, res, next, new DBManager(CONFIG))
-},
-Helper.verifyAuthToken, 
-async (req, res) =>{
-    try{
-        let currentUser = await Helper.jwtVerifyUser(req.token, publicKey)
+api.get("/ledgers",
+    async(req, res, next) => {
+        await Helper.verifyRequest(req, res, next, new DBManager(CONFIG))
+    },
+    Helper.verifyAuthToken,
+    async(req, res) => {
         let db = new DBManager(CONFIG)
-        // let ledgers = await User.get_all_ledgers(currentUser.user_id, db)
-        let ledgers = [1, 2, 3]
-        // get user data -> ledgers and shit and merge it into user object
-        return res.json(ledgers)
-    }catch(e){
-        if("get" in e){
-            return res.status(400).json({error: e.get()})    
+        try {
+            let currentUser = await Helper.jwtVerifyUser(req.token, publicKey)
+            let ledgers = await Ledger.get_all(currentUser.user_id, db)
+                // get user data -> ledgers and shit and merge it into user object
+
+            return res.json(ledgers.get())
+        } catch (e) {
+            if ("get" in e) {
+                return res.status(400).json({ error: e.get() })
+            }
+            console.log(e)
+            return res.status(400).json({ error: "Invalid Request." })
+        } finally {
+            db.con.end()
         }
-        console.log(e)
-        return res.status(400).json({error: "Invalid Request."})
     }
-}
 )
 
 
 
 // create a new ledger
-api.post("/ledger", 
-async (req, res, next) =>{
-    await Helper.verifyRequest(req, res, next, new DBManager(CONFIG))
-},
-Helper.verifyAuthToken, 
-async (req, res) =>{
-    try{
-        let currentUser = await Helper.jwtVerifyUser(req.token, publicKey)
+api.post("/ledger",
+    async(req, res, next) => {
+        await Helper.verifyRequest(req, res, next, new DBManager(CONFIG))
+    },
+    Helper.verifyAuthToken,
+    async(req, res) => {
         let db = new DBManager(CONFIG)
-        let {user_id} = currentUser
-        let {ledger_name, users} = req.body
+        try {
+            let currentUser = await Helper.jwtVerifyUser(req.token, publicKey)
+            let { user_id } = currentUser
+            let { ledger_name, users } = req.body
+            let userIDs = []
 
+            for await (const u of users) {
+                let ux = await User.get_id(u, db)
+                if (ux.user_id)
+                    userIDs.push(ux.user_id)
+            }
+            userIDs.push(user_id)
+            let result = await Ledger.create(ledger_name, user_id, userIDs, db)
+            let ledger_id = result.get()
+            return res.json({ ledger_id, success: true })
 
-        // get user data -> ledgers and shit and merge it into user object
-        return res.json({ledger_name, users})
-    }catch(e){
-        if("get" in e){
-            return res.status(400).json({error: e.get()})    
+        } catch (e) {
+            if ("get" in e) {
+                return res.status(400).json({ error: e.get() })
+            }
+            console.log(e)
+            return res.status(400).json({ error: "Invalid Request." })
+        } finally {
+            db.con.end()
         }
-        console.log(e)
-        return res.status(400).json({error: "Invalid Request."})
     }
-}
 )
 
 
 
-api.get("/ledger/:id", 
-async (req, res, next) =>{
-    let r = req
-    r.url = "/ledger/:id"
-    await Helper.verifyRequest(r, res, next, new DBManager(CONFIG))
-},
-Helper.verifyAuthToken,
-async (req, res) =>{
-    try{
-        let ledger = "sample ledger"
-        // get user data and merge it into user object
-        return res.json({ledger})
-    }catch(e){
-        if("get" in e){
-            return res.status(400).json({error: e.get()})    
+api.get("/ledger/:id",
+    async(req, res, next) => {
+        let r = req
+        r.url = "/ledger/:id"
+        await Helper.verifyRequest(r, res, next, new DBManager(CONFIG))
+    },
+    Helper.verifyAuthToken,
+    async(req, res) => {
+        try {
+            let ledger = "sample ledger"
+                // get user data and merge it into user object
+            return res.json({ ledger })
+        } catch (e) {
+            if ("get" in e) {
+                return res.status(400).json({ error: e.get() })
+            }
+            console.log(e)
+            return res.status(400).json({ error: "Invalid Request." })
         }
-        console.log(e)
-        return res.status(400).json({error: "Invalid Request."})
     }
-}
 )
 
 
